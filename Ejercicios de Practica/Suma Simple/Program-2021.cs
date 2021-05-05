4d36b7e0e5cbcfe7610e981311bf48f77178da15
Alexander Escalona Fernandez <aescalona@level6.co>
2021-05-04 23:52:38 -0400

    add Suma Simple practice exercise

---
+++ Ejercicios de Practica/Suma Simple/Program-2021.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumaSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int n;
+            while ((line = Console.ReadLine()) != null)
+            {
+                n = int.Parse(line.Trim());
+                Console.WriteLine(n * (n + 1) / 2);
+            }
+        }
+    }
+}
