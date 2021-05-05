b628117f018031ee28c449a2b9e7cfcc4e8975d9
Alexander Escalona Fernandez <aescalona@level6.co>
2021-05-04 23:49:01 -0400

    add Suma de Digitos practice exercise

---
+++ Ejercicios de Practica/Suma de Dígitos/Program-2021.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace SumaDigitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(line.Trim().Select(e => e - '0').Sum());
+            }
+        }
+    }
+}
