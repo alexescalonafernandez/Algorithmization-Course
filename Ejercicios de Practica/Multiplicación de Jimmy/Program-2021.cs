61635fe7996000debdff8eee1b37062b6015b090
Alexander Escalona Fernandez <aescalona@level6.co>
2021-05-04 23:02:58 -0400

    add Multiplicación de Jimmy practice exercise

---
+++ Ejercicios de Practica/Multiplicación de Jimmy/Program-2021.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace MultiplicacionJimmy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine()), secondSum;
+            string[] arr;
+            int[] first;
+            while (cases-- > 0)
+            {
+                arr = Console.ReadLine().Split();
+                first = arr[0].Select(c => c - '0').ToArray();
+                secondSum = arr[1].Select(c => c - '0').Sum();
+                Console.WriteLine(first.Sum(e => e * secondSum));
+
+            }
+        }
+    }
+}
