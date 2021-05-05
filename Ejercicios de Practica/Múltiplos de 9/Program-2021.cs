14978e84f74f7a2b772cf4265937c9bcbc81f37b
Alexander Escalona Fernandez <aescalona@level6.co>
2021-05-04 23:17:05 -0400

    add Multiplos de 9 practice exercise

---
+++ Ejercicios de Practica/Múltiplos de 9/Program-2021.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Multiplos9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line, store;
+            int degree, sum;
+            while ((line = Console.ReadLine()) != "0")
+            {
+                sum = line.Select(e => e - '0').Sum();
+                if (sum % 9 != 0)
+                {
+                    Console.WriteLine($"{line} is not a multiple of 9.");
+                    continue;
+                }
+
+                if (line.Length == 1)
+                {
+                    Console.WriteLine("9 is a multiple of 9 and has 9-degree 1.");
+                }
+                else
+                {
+                    store = line;
+                    line = $"{sum}";
+                    degree = 1;
+                    do
+                    {
+                        
+                        sum = line.Select(e => e - '0').Sum();
+                        degree++;
+                        line = $"{sum}";
+                    } while (line.Length > 1) ;
+                    Console.WriteLine($"{store} is a multiple of 9 and has 9-degree {degree}.");
+                }
+            }
+        }
+    }
+}
