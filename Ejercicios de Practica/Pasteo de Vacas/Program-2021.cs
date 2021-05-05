1eb3ce150eacc15c8b172384d36e8dc65c9cc453
Alexander Escalona Fernandez <aescalona@level6.co>
2021-05-04 23:45:40 -0400

    add Pasteo de Vacas practice exercise

---
+++ Ejercicios de Practica/Pasteo de Vacas/Program-2021.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace PasteoVacas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine()), consecutives, max;
+            while (cases-- > 0)
+            {
+                Console.ReadLine();
+                var cows = Console.ReadLine().Split().Select(e => int.Parse(e));
+                max = consecutives = 0;
+                foreach (var e in cows)
+                {
+                    if (e % 2 == 0)
+                        consecutives++;
+                    else
+                    {
+                        max = Math.Max(max, consecutives);
+                        consecutives = 0;
+                    }
+                }
+                Console.WriteLine(Math.Max(max, consecutives));
+            }
+        }
+    }
+}
