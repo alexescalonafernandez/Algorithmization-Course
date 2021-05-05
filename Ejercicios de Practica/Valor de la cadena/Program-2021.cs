02413bb8819ca5bfc122c13c76a4eae6d61d25b3
Alexander Escalona Fernandez <aescalona@level6.co>
2021-05-04 23:55:53 -0400

    add Valor de la cadena practice exercise

---
+++ Ejercicios de Practica/Valor de la cadena/Program-2021.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ValorCadena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(line.Select(e => (int) e).Sum());
+            }
+        }
+    }
+}
