7f45eea343a94479f40e9be2efdc1c1bde7e992f
Alexander Escalona Fernandez <aescalona@level6.co>
2021-05-05 00:08:30 -0400

    add Verbally successive sequence practice exercise

---
+++ Ejercicios de Practica/Verbally successive sequence/Program-2021.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VerballySuccessiveSequence
+{
+    class Program
+    {
+        class Token
+        {
+            public char Value { get; set; }
+            public int Count { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            var cases = int.Parse(Console.ReadLine());
+            char[] arr;
+            Token current;
+            while (cases-- > 0)
+            {
+                arr = Console.ReadLine().Trim().ToCharArray();
+                current = null;
+                var tokens = new List<Token>();
+                for (var i = 0; i < arr.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        current = new Token
+                        {
+                            Value = arr[i],
+                            Count = 1
+                        };
+                    }
+                    else if (current?.Value == arr[i])
+                    {
+                        current.Count++;
+                    }
+                    else
+                    {
+                        tokens.Add(current);
+                        current = new Token
+                        {
+                            Value = arr[i],
+                            Count = 1
+                        };
+                    }
+                }
+                tokens.Add(current);
+                var solution = string.Join("", tokens.Select(e => $"{e.Count}{e.Value}"));
+                Console.WriteLine(solution);
+            }
+        }
+    }
+}
