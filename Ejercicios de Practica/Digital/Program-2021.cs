6dd35a466f30dcc3861dabe805964ed8ab039116
Alexander Escalona Fernandez <aescalona@level6.co>
2021-05-04 22:46:44 -0400

    add Digital practice exercise

---
+++ Ejercicios de Practica/Digital/Program-2021.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Digital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            while ((number = Console.ReadLine()) != null)
+            {
+                while (number.Length > 1)
+                {
+                    number = $"{number.Select(e => e - '0').Sum()}";
+                }
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
